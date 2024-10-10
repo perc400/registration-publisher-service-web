873375ec46127f27207604f33739f1db2e79d338
Administrator <gitlab_admin_4b656f@example.com>
2024-10-10 20:05:20 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using WebAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 string connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION")
-		?? builder.Configuration.GetConnectionString("PostgreSqlProvider");
+	?? builder.Configuration.GetConnectionString("PostgreSqlProvider");
 builder.Services.AddDbContext<WebApiContext>(options =>
             options.UseNpgsql(connectionString)
         );
@@ -14,6 +14,12 @@ System.Console.WriteLine($"Connection string: {connectionString}");
 
 var app = builder.Build();
 app.UseRouting();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<WebApiContext>();
+    db.Database.Migrate();
+}
 
 #pragma warning disable ASP0014
 app.UseEndpoints(endpoints =>
